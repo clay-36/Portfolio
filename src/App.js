1da163fc04ba4c7bc5f7caff3a6561d566d2fa22
clay-36 <ravibabriya2392@gmail.com>
2020-05-23 14:22:55 +0530

    Name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home'
 
 function App() {
   return (
     <div className="App">
-     
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Home.css'
+
+export default class Home extends React.Component {
+
+
+
+    render(){
+        return (
+            <div className="brand-name">
+                <span className="R1">R</span>
+                <span className="A2">A</span>
+                <span className="V3">V</span>
+                <span className="I4">I</span>
+                <span className="S5">  &#8203;</span>
+                <span className="B6">B</span>
+                <span className="A7">A</span>
+                <span className="B8">B</span>
+                <span className="R9">R</span>
+                <span className="I10">I</span>
+                <span className="Y11">Y</span>
+                <span className="A12">A</span>
+                <span className="A13">.</span>
+                <span className="A14">.</span>
+               </div>
+        );
+    }
+}
