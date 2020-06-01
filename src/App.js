5fff240dc067830bc70fc8a2f7a663d12f3c0c68
clay-36 <ravibabriya2392@gmail.com>
2020-06-01 16:29:46 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home'
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Home />
     </div>
   );
---
+++ src/Components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import './Header.css'
+
+
+export default class Header extends React.Component{
+    render(){
+        const { Header, Content, Footer } = Layout;
+
+        return(
+            <Layout className="layout">
+            <Header>
+              <div className="logo" />
+              <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
+              </Menu>
+            </Header>
+          </Layout>
+        );
+    }
+}
+
